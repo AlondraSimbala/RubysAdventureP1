6c4843f2253314799857bfdd395d0ff6b7b5d940
AlondraSimbala <alondrasimbala@gmail.com>
2022-12-09 09:59:21 -0800

    Added projectile and a new animation for robot
    added projectile (clog) added a fixed animation for Enemy1

--- RubysAdventureSimbala/Assets/Scripts/Projectile.cs
+++ RubysAdventureSimbala/Assets/Scripts/Projectile.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D rigidbody2d;
+
+    void Awake()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Enemy1Controller e = other.collider.GetComponent<Enemy1Controller>();
+        if (e != null)
+        {
+            e.Fix();
+
+        }
+
+        Destroy(gameObject);
     }
 }
